7b38cc877de5850e9ef7ad20ed31d8262f5301d1
anusha <anushakizhakkeveedu@gmail.com>
2023-12-18 13:47:09 +0530

    Added employee leave submission form

--- src/components/Employees/employeespage.js
+++ src/components/Employees/employeespage.js
@@ -4,17 +4,39 @@ import './Style.css';
 export default function Employees() {
     const [employees, setEmployees] = useState([]);
     const [employeeDetail, setEmployeeDetail] = useState({});
+    const [leaveDetails, setLeaveDetails] = useState({
+        leave_date: '',
+        leave_reason: '',});
 
     const fetchData = async () => {
         const response = await fetch("http://localhost:5000/employees");
         const jsonData = await response.json();
-        setEmployees(jsonData);};
+        setEmployees(jsonData); };
 
     useEffect(() => {
-        fetchData();}, []);
+        fetchData(); }, []);
 
     const handleEmployeeClick = (index) => {
-        setEmployeeDetail(employees[index]);};
+        setEmployeeDetail(employees[index]); };
+
+    const handleLeaveDetailsChange = (e) => {
+        setLeaveDetails((prev) => ({ ...prev,
+            [e.target.name]: e.target.value,})); };
+
+    const handleSubmitLeave = async () => {
+       
+            const response = await fetch(`http://localhost:5000/leaves/${employeeDetail.id}`, {
+                method: "POST",
+                body: JSON.stringify(leaveDetails),
+                headers: {
+                    'Content-Type': 'application/json'  } });
+            if (response.status === 200) {
+                const jsonData = await response.json();
+                alert(jsonData.message);
+                setLeaveDetails({ leave_date: '', leave_reason: '', });
+            }
+            else {
+                 }   };
 
     return (
         <>
@@ -23,7 +45,8 @@ export default function Employees() {
                 {employees.map((data, index) => (
                     <p key={index} onClick={() => handleEmployeeClick(index)}>
                         {index + 1}. {data.fname} {data.lname}
-                    </p>))}
+                    </p>
+                ))}
             </div>
             {employeeDetail && (
                 <div>
@@ -45,8 +68,42 @@ export default function Employees() {
                         <p>{employeeDetail.max_leaves}</p>
                         <p>{employeeDetail.remaining_leaves}</p>
                     </div>
-                </div> )}
+                </div>
+            )}
+
+            <div className="add-leave-container">
+                <h4>Add Leave Details</h4>
+                <form id="leaveForm" className="input-form">
+                    <div className="input-label">
+                        <label htmlFor="leave_date">Leave Date</label>
+                        <input
+                            value={leaveDetails.leave_date}
+                            type="date"
+                            id="leave_date"
+                            name="leave_date"
+                            onChange={handleLeaveDetailsChange}
+                        />
+                        <br />
+                        <br />
+                    </div>
+
+                    <div className="input-field">
+                        <label htmlFor="leave_reason">Leave Reason</label>
+                        <input
+                            value={leaveDetails.leave_reason}
+                            type="text"
+                            id="leave_reason"
+                            name="leave_reason"
+                            onChange={handleLeaveDetailsChange}
+                        />
+                        <br />
+                        <br />
+                    </div>
+                </form>
+                <button className="submit-button" onClick={handleSubmitLeave}>
+                    Submit
+                </button>
+            </div>
         </>
     );
 }
-
