5512de11868124c3c47e1ff79188549d11775c7c
anusha <anushakizhakkeveedu@gmail.com>
2023-12-18 12:45:08 +0530

    load employee list from database

--- src/components/Employees/employeespage.js
+++ src/components/Employees/employeespage.js
@@ -1,5 +1,23 @@
+import './Style.css';
+
+
+import React, { useState, useEffect } from 'react';
 export default function Employees() {
+    const [employees, setEmployees] = useState([]);
+    const fetchData = async () => {
+  const response = await fetch("http://localhost:5000/employees");
+        const jsonData = await response.json();
+        setEmployees(() => jsonData);
+    console.log(employees)
+    };
+    useEffect(() => {
+        fetchData()
+    },)
     return (
-        <h1>employee page</h1>
-    )
-};
+        <>
+            <h3 className="list-of-employees">List of Employees</h3>
+            <div className="parent">
+                {employees.map(data => <p>{data.fname} { data.lname}</p>)}
+            </div>
+            </>    )
+}
