c192d7f9a4d36b10d5d9626c0cedd371930d42e9
anusha <anushakizhakkeveedu@gmail.com>
2023-12-18 13:20:21 +0530

    Implemented employee detail selection on click

--- src/components/Employees/employeespage.js
+++ src/components/Employees/employeespage.js
@@ -1,25 +1,36 @@
-import './Style.css';
-
 import React, { useState, useEffect } from 'react';
+import './Style.css'; 
+
+
 export default function Employees() {
     const [employees, setEmployees] = useState([]);
+    const [employeeDetail, setEmployeeDetail] = useState({});
+
     const fetchData = async () => {
-  const response = await fetch("http://localhost:5000/employees");
+        const response = await fetch("http://localhost:5000/employees");
         const jsonData = await response.json();
-        setEmployees(() => jsonData);
-    console.log(employees)
-    };
+        setEmployees(jsonData);};
+
     useEffect(() => {
-        fetchData()
-    },)
+        fetchData();}, []);
+
+    const handleEmployeeClick = (index) => {
+        setEmployeeDetail(employees[index]); };
+
     return (
         <>
             <h3 className="list-of-employees">List of Employees</h3>
             <div className="parent">
-            {employees.map((data, index) => (
-                <p key={index}>{index + 1}. {data.fname} {data.lname}</p>))}
+                {employees.map((data, index) => (
+                    <p key={index} onClick={() => handleEmployeeClick(index)}>
+                        {index + 1}. {data.fname} {data.lname}
+                    </p> ))}
             </div>
+            {employeeDetail && (
+                <div>
+                    <h1>{employeeDetail.fname}</h1>
+                </div>
+            )}
         </>
-    )
+    );
 }
-
