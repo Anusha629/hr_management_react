72fbc8cb25ea0e52c58b2e4d3cb6e8652b8c430c
anusha <anushakizhakkeveedu@gmail.com>
2023-12-20 21:52:54 +0530

    Add error handling page

---
+++ src/components/Error/errorpage.js
@@ -0,0 +1,12 @@
+import './error.css'
+
+export default function ErrorPage() {
+    return (
+        <div className="error-page">
+            <h1>404</h1>
+            <p className="error-text">
+                Not Found
+            </p>
+        </div>
+    )
+}
