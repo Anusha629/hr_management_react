e75ec72383bac26aa323b177df0ad4fd940bce86
anusha <anushakizhakkeveedu@gmail.com>
2024-01-01 10:54:42 +0530

    import components arranged

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Nav from './components/Header/index.js';
 import Home from './components/Home/homepage.js';
 import Employees from './components/Employees/employeespage.js';
 import About from './components/About/aboutpage.js';
+import ErrorPage from './components/Error/errorpage';
 
 import { Route, Routes} from 'react-router-dom';
-import ErrorPage from './components/Error/errorpage';
 
 function App() {
   return (
